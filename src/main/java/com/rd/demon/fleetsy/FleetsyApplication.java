45e53c4855e3f7789864ee9078463706e6fae785
RD <rory.c.dickinson@gmail.com>
2020-10-24 14:12:12 -0500

    init

---
+++ src/main/java/com/rd/demon/fleetsy/FleetsyApplication.java
@@ -0,0 +1,13 @@
+package com.rd.demon.fleetsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetsyApplication.class, args);
+	}
+
+}
